0b74538516826a29e904cb4dcc997a8a0ac39e21
RaphaelT <raphael.tetreault@mohawkcollege.ca>
2026-03-31 18:00:45 -0400

    Add player and drop discs (prefab)

---
+++ w26-unity-plinko/Assets/Scripts/Player.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public float speed = 5;
+    public GameObject discPrefab;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Move player horizontally
+        float moveX = Input.GetAxis("Horizontal") * Time.deltaTime * speed;
+        Vector3 position = transform.position;
+        position.x += moveX;
+        transform.position = position;
+
+        // Drop disc
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            // Clone prefab
+            Instantiate(discPrefab, transform.position, Quaternion.identity);
+        }
+    }
+}
